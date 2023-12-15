50da3928919ad851bde454f7e4db6bc33dfedba0
Akshit Gupta <akshit0689@gmail.com>
2023-12-15 22:05:04 +0530

    Message Sender, feed

---
+++ fb-clone/src/MessageSender.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./MessageSender.css";
+import Avatar from '@mui/material/Avatar';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+
+function MessageSender() {
+    const handleSubmit = e => {
+        e.preventDefault(); //prevents the page to refresh
+    }
+  return (
+    <div className='messageSender'>
+        <div className='messageSender__top'>
+            <Avatar />
+            <form>
+                <input type = "text" className='messageSender__input'
+                placeholder="What's on your mind"/>
+                <input placeholder='Image URL (Optional)' />
+
+                <button onClick={handleSubmit} type="submit" > 
+                    Hidden Submit
+                </button> 
+
+            </form>
+        </div>
+        <div className='messageSender__bottom'>
+            <div className='messageSender__option'>
+                <VideocamIcon style= {{ color: "red" }} />
+                <h3>Live Video</h3>
+            </div>
+
+            <div className='messageSender__option'>
+                <PhotoLibraryIcon style = {{ color: "green" }} />
+                <h3>Photo/Video</h3>
+            </div>
+            <div className='messageSender__option'>
+                <InsertEmoticonIcon style = {{ color: "orange" }} /> 
+                <h3>Feeling/Activity</h3>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MessageSender
