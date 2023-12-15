7f773b48e6b1c4dd93635f93561ab3912c93a6d4
Akshit Gupta <akshit0689@gmail.com>
2023-12-15 17:36:55 +0530

    Feed

---
+++ fb-clone/src/Feed.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Feed.css"; 
+import StoryReel from './StoryReel';
+
+function Feed() {
+  return (
+    <div className='feed'>Feed
+    <StoryReel />
+    {/* StoryReel */}
+    {/* MessageSender */}
+
+    </div>
+  )
+}
+
+export default Feed
