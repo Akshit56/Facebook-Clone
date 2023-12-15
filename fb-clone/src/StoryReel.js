0febb69497301d36afc014b12ed372be6eba8d53
Akshit Gupta <akshit0689@gmail.com>
2023-12-15 17:36:45 +0530

    fb story

---
+++ fb-clone/src/StoryReel.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './StoryReel.css'
+import './Story.css';
+import Story from './Story';
+
+function StoryReel() {
+  return (
+    <div className='storyReel'> 
+        <Story 
+            image="https://gist.github.com/brettlangdon/85942af486eb79118467/raw/2a7409cd3c26a90b2e82bdc40dc7db18b92b3517/01151_inthedeep_2560x1600.jpg"
+            profileSrc="https://avatars.githubusercontent.com/u/92316352?s=400&u=bfe5bef0337f1f95dd9e94dfa2fec5be65407114&v=4"
+            title="Akshit Gupta"
+        />
+        
+        <Story 
+            image="https://gist.github.com/brettlangdon/85942af486eb79118467/raw/2a7409cd3c26a90b2e82bdc40dc7db18b92b3517/01151_inthedeep_2560x1600.jpg"
+            profileSrc="https://avatars.githubusercontent.com/u/92316352?s=400&u=bfe5bef0337f1f95dd9e94dfa2fec5be65407114&v=4"
+            title="Akshit Gupta"
+        />
+        
+        <Story 
+            image="https://gist.github.com/brettlangdon/85942af486eb79118467/raw/2a7409cd3c26a90b2e82bdc40dc7db18b92b3517/01151_inthedeep_2560x1600.jpg"
+            profileSrc="https://avatars.githubusercontent.com/u/92316352?s=400&u=bfe5bef0337f1f95dd9e94dfa2fec5be65407114&v=4"
+            title="Akshit Gupta"
+        />
+        
+        <Story 
+            image="https://gist.github.com/brettlangdon/85942af486eb79118467/raw/2a7409cd3c26a90b2e82bdc40dc7db18b92b3517/01151_inthedeep_2560x1600.jpg"
+            profileSrc="https://avatars.githubusercontent.com/u/92316352?s=400&u=bfe5bef0337f1f95dd9e94dfa2fec5be65407114&v=4"
+            title="Akshit Gupta"
+        /> 
+        {/* Story */}
+        {/* Story */}
+        {/* Story */}
+        {/* Story */}
+
+    </div>
+  )
+}
+
+export default StoryReel
